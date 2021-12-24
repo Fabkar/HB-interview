c699eea286810647954a40bed46507e6c2d0f51f
Fabkar <fab_862@hotmail.com>
2021-12-23 20:49:04 -0500

    fixxing

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,5 +1,8 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,18 +12,30 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
+
+/**
+ * struct queue - A linked list representing queues.
+ *
+ * @node: heap_t type node (binary tree node)
+ * @next: pointer to the next node
+ */
+typedef struct queue
+{
+	heap_t *node;
+	struct queue *next;
+} queue_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
