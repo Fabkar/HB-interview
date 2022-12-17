010575f297444f5c1663389811a469382c4ff2b3
FabianCV <FabianCarmona@comfama.com.co>
2022-12-17 12:50:30 -0500

    making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,42 +1,23 @@
 #!/usr/bin/python3
+"""Making Change problem
 """
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
-"""
-from collections import deque
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-    Return: fewest number of coins needed to meet total
-        - If total is 0 or less, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins in your possession
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination of
-        coin in the list
-    Using a Breadth-First Search top-down approach.
+    """Returns the fewest number of coins needed to meet a given total
+    Args:
+        coins: array of integers representing the coin values
+        total: the total to find coins for
     """
     if total <= 0:
         return 0
 
-    memo = set()
-    tree_queue = deque([[0, 0]])
+    array = [float('inf')] * (total + 1)
+    array[0] = 0
 
-    while tree_queue:
-        current, level = tree_queue.popleft()
+    for i in range(1, len(array)):
+        for j in range(len(coins)):
+            if coins[j] <= i:
+                array[i] = min(array[i], array[i - coins[j]] + 1)
 
-        if level != 0 and current == total:
-            return level
-
-        for coin in coins:
-            if current + coin not in memo and current + coin <= total:
-                tree_queue.append([current + coin, level + 1])
-                memo.add(current + coin)
-
-    return -1
-    
+    return array[i] if array[i] != float('inf') else -1
