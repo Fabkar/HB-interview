573a49b1ddf8f17b196eba50754da6fca044d4b7
FabianCV <FabianCarmona@comfama.com.co>
2022-12-16 19:09:46 -0500

    I cant belive

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
-""" module doc """
+"""
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
+"""
+from collections import deque
 
 
-def makeChange(cs, ttl):
+def makeChange(coins, total):
     """
-    function doc
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
+    Using a Breadth-First Search top-down approach.
     """
-    if ttl <= 0:
+    if total <= 0:
         return 0
-    cs.sort(reverse=True)
-    s = 0
-    i = 0
-    ct = 0
-    n_c = len(cs)
-    while s < ttl and i < n_c:
-        while cs[i] <= ttl - s:
-            s += cs[i]
-            ct += 1
-            if s == ttl:
-                return ct
-        i += 1
+
+    memo = set()
+    tree_queue = deque([[0, 0]])
+
+    while tree_queue:
+        current, level = tree_queue.popleft()
+
+        if level != 0 and current == total:
+            return level
+
+        for coin in coins:
+            if current + coin not in memo and current + coin <= total:
+                tree_queue.append([current + coin, level + 1])
+                memo.add(current + coin)
+
     return -1
     
