33822492af90574cb3da1298ec976d5805d845bd
FabianCV <FabianCarmona@comfama.com.co>
2022-12-16 19:18:21 -0500

    testing regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,121 +1,40 @@
 #include "regex.h"
-/* fprintf */
-#include <stdio.h>
-
 
 /**
- * regexSubstrMatch - when in an open wildcard state after a `.*` in the regex
- *   pattern, regexSubstrMatch is called as a helper to regex_match to
- *   determine if there is a substring in str matching the substring in
- *   pattern immediately following the wildcard
- *
+ * regex_match - checks where given pattern matches a given string
  * @str: string to scan
- *   - can be empty, or contain any ASCII values other than '.' or '*'
- * @pattern: regular expression pattern to match
- *   - can be empty, or contain any ASCII values
- * @s_match_end: pointer to first index in str after the **last** instance of a
- *   matching substring
- * @p_match_end: pointer to the first index in pattern after a substring
- *   bounded by an open wildcard to the left, and another wildcard or the end
- *   of the pattern on the right
+ * @pattern: regluar expression
  *
- * Return: 1 if matching substring found after wildcard, or 0 if not or on
- *   failure
- */
-int regexSubstrMatch(char const *str, char const *pattern,
-		     const char **s_match_end, const char **p_match_end)
-{
-	const char *p_match_start;
-	int match = 0;
-
-	if (!str || !pattern)
-	{
-		fprintf(stderr, "regexSubstrMatch: NULL parameter(s)\n");
-		return (0);
-	}
-
-	while (*str)
-	{
-		/* skip past any chars not matching pattern after `.*` */
-		while (*str && *str != *pattern)
-			str++;
-
-		p_match_start = pattern;
-
-		/* attempt to match exit substring */
-		while (*str == *pattern && *pattern != '.' &&
-		       *pattern != '*' && *pattern != '\0' &&
-		       *(pattern + 1) != '*')
-		{
-			str++;
-			pattern++;
-		}
-
-		/* record end of current substr match in str */
-		if (*pattern == '.' || *pattern == '*' ||
-		    *pattern == '\0' || *(pattern + 1) == '*')
-		{
-			*p_match_end = pattern;
-			*s_match_end = str;
-			match = 1;
-		}
-
-		pattern = p_match_start;
-	}
-
-	return (match);
-}
-
-
-/**
- * regex_match - checks whether a given pattern matches a given string
- *
- * @str: string to scan
- *   - can be empty, or contain any ASCII values other than '.' or '*'
- * @pattern: regular expression pattern to match
- *   - can be empty, or contain any ASCII values
- *
- * Return: 1 on successful match, 0 if not or on failure
+ * Return: 1 if pattern matches, 0 if doesn't
  */
 int regex_match(char const *str, char const *pattern)
 {
-	char repeat;
-	const char *s_match_end = NULL, *p_match_end = NULL;
+	unsigned int i = 0, j = 0;
 
-	if (!str || !pattern)
+	for (i = 0, j = 0; str[i]; i++, j++)
 	{
-		fprintf(stderr, "regex_match: NULL parameter(s)\n");
-		return (0);
-	}
-
-	while (*pattern)
-	{
-		if (*(pattern + 1) == '*')
+		if (str[i] == pattern[j])
+			continue;
+		else if (pattern[j] == '.')
+			continue;
+		else if (pattern[j] == '*')
 		{
-			if (*pattern == '.')
+			if (pattern[j - 1] == '.')
 			{
-				pattern += 2;
-				if (regexSubstrMatch(str, pattern, &s_match_end, &p_match_end))
-				{
-					str = s_match_end;
-					pattern = p_match_end;
-				}
+				while (pattern[j + 1] != str[i] && str[i])
+					i++;
 			}
 			else
 			{
-				repeat = *pattern;
-				pattern += 2;
-				while (*str == repeat)
-					str++;
+				while (pattern[j - 1] == str[i])
+					i++;
 			}
+			i--;
 		}
-		else if (*pattern == '.' || *pattern == *str)
-		{
-			pattern++;
-			str++;
-		}
+		else if (pattern[j + 1] && pattern[j + 1] == '*')
+			i--;
 		else
-			break;
+			return (0);
 	}
-	return (!(*str) && !(*pattern));
+	return (1);
 }
